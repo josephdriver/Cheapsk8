0b1393b26630e1a70d29b8716c0551dbf3b7fd7a
Joe Driver <josephdriver@Joes-MacBook-Pro-2.local>
2022-07-24 21:41:12 +1000

    Implemented basic splash and axios request

---
+++ utilities/useAxiosFetch.js
@@ -0,0 +1,58 @@
+import { useState, useEffect, useRef } from 'react';
+import Axios from 'axios';
+import { get } from 'lodash';
+
+const useAxiosFetch = (
+    url,
+    dataDefault = null,
+    timeout,
+    refetch = false,
+    loadOnMount = true,
+    searchParams = false
+) => {
+    const [data, setData] = useState(dataDefault);
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading, setLoading] = useState(loadOnMount);
+    const initialLoadDone = useRef(false);
+    useEffect(() => {
+        let unmounted = false;
+        const source = Axios.CancelToken.source();
+        if ((!initialLoadDone.current && loadOnMount) || refetch) {
+            setLoading(true);
+            const options = {
+                cancelToken: source.token,
+                timeout,
+            };
+            if (searchParams) {
+                options.params = searchParams;
+            }
+            Axios.get(url, options)
+                .then((a) => {
+                    initialLoadDone.current = true;
+                    if (!unmounted) {
+                        setData(a.data);
+                        setLoading(false);
+                    }
+                })
+                .catch((e) => {
+                    initialLoadDone.current = true;
+                    if (!unmounted) {
+                        setLoading(false);
+                        if (!Axios.isCancel(e)) {
+                            setError(true);
+                            setErrorMessage(get(e, 'response.data.message', e.message));
+                        }
+                    }
+                });
+        }
+        return () => {
+            unmounted = true;
+            source.cancel();
+        };
+    }, [timeout, url, refetch, loadOnMount, searchParams]);
+
+    return { data, loading, error, errorMessage };
+};
+
+export default useAxiosFetch;
