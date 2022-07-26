459d3617ebd5d7eb8524de09d2525cbc460e33da
Joe Driver <josephdriver@Joes-MacBook-Pro-2.local>
2022-07-26 17:05:14 +1000

    implemented react native vector icons

--- screens/Featured.js
+++ screens/Featured.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-const Featured = () => {
-    return (
-        <View>
-            <Text>Featured</Text>
-        </View>
-    );
-};
+function Featured() {
+  return (
+    <View>
+      <Text>Featured</Text>
+      <Icon name="rocket" size={30} color="#900" />
+    </View>
+  );
+}
 export default Featured;
