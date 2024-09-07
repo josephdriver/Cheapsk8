2fac6d7ec1139d28146e9204364fe21720b0f5d9
Joe Driver <joseph.driver0@gmail.com>
2024-09-07 15:58:51 +1000

    Changed app name

--- ios/Cheapsk8teTests/Cheapsk8teTests.m
+++ ios/Cheapsk8teTests/Cheapsk8teTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface StingyTests : XCTestCase
+@interface Cheapsk8teTests : XCTestCase
 
 @end
 
-@implementation StingyTests
+@implementation Cheapsk8teTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
